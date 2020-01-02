3e1e0abc47e025932a7789c9f4beee3c8d6079aa
Dominic Shelton <frogamic@gmail.com>
2020-01-02 11:37:14 +1100

    Move move fizzbuzz string coercion solution to folder

---
+++ fizzbuzz/string_coercion.js
@@ -0,0 +1,12 @@
+const fizzbuzz = i =>
+  ({
+    ['true,true']: 'FizzBuzz',
+    ['true,false']: 'Fizz',
+    ['false,true']: 'Buzz',
+    ['false,false']: i
+  }[[!(i % 3), !(i % 5)]]);
+
+[...Array(100).keys()]
+  .map(x => x + 1)
+  .map(fizzbuzz)
+  .map(x => console.log(x));
