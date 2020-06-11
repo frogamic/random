59e6c3f3813a264a72dd6a7e80d7a9764590721b
Dominic Shelton <frogamic@gmail.com>
2020-06-11 15:59:37 +1000

    Refactor reduction

--- fizzbuzz/reduction.js
+++ fizzbuzz/reduction.js
@@ -1,27 +1,26 @@
 #!/usr/bin/env node
 
-const wordList = [
-  [3, 'Fizz'],
-  [5, 'Buzz'],
-  // [7, 'Quux'], // I know you'll want this...
+const conditions = [
+  { divisible: 3, word: 'Fizz' },
+  { divisible: 5, word: 'Buzz' }
+  // {divisible: 7, word: 'Quux'}, // I know you'll want this...
 ];
 
-const print = x => console.log(x);
-const plus = x => y => x + y;
-
-const makeWordifier = (divisible, word) => (acc, i) =>
+const makeWordifier = ({ divisible, word }) => (acc, i) =>
   i % divisible === 0 ? `${acc}${word}` : acc;
 
 const fallback = (acc, i) => (acc === '' ? String(i) : acc);
 
-const wordReducer = x => {
-  return wordList
-    .map(x => makeWordifier(...x))
+const fizzBuzz = num =>
+ conditions 
+    .map(x => makeWordifier(x))
     .concat(fallback)
-    .reduce((acc, f) => f(acc, x), '');
-};
+    .reduce((acc, f) => f(acc, num), '');
 
-[...Array(105).keys()]
+const print = x => console.log(x);
+const plus = x => y => x + y;
+
+[...Array(100).keys()]
   .map(plus(1))
-  .map(wordReducer)
+  .map(fizzBuzz)
   .map(print);
--- fizzbuzz/string_coercion.js
+++ fizzbuzz/string_coercion.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const fizzbuzz = i =>
   ({
     ['true,true']: 'FizzBuzz',
